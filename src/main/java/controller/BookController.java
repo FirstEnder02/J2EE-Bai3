42ba6873cb89524603538754a59ea26d5880f76c
Huy <tranhauhuy265@gmail.com>
2026-02-03 13:55:13 +0700

    Commit 1

--- src/main/java/controller/BookController.java
+++ src/main/java/controller/BookController.java
@@ -1,9 +1,10 @@
 package controller;
 
-import model.Book;
-import service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import model.Book;
+import service.BookService;
 
 import java.util.List;
 
@@ -12,24 +13,29 @@ import java.util.List;
 public class BookController {
     @Autowired
     private BookService bookService;
+
     @GetMapping
     public List<Book> getAllBooks() {
         return bookService.getAllBooks();
     }
+
     @GetMapping("/{id}")
     public Book getBookById(@PathVariable int id) {
         return bookService.getBookById(id);
     }
+
     @PostMapping
     public String addBook(@RequestBody Book book) {
         bookService.addBook(book);
         return "Book added successfully!";
     }
+
     @PutMapping("/{id}")
     public String updateBook(@PathVariable int id, @RequestBody Book updatedBook) {
         bookService.updateBook(id, updatedBook);
         return "Book updated successfully!";
     }
+
     @DeleteMapping("/{id}")
     public String deleteBook(@PathVariable int id) {
         bookService.deleteBook(id);
--- src/main/java/service/BookService.java
+++ src/main/java/service/BookService.java
@@ -1,37 +1,12 @@
 package service;
 
 import model.Book;
-import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
 import java.util.List;
 
-@Service
-public class BookService {
-    private List<Book> books = new ArrayList<>();
-    
-    public void addBook(Book book) {books.add(book);}
-
-    public List<Book> getAllBooks() {return books;}
-
-    public Book getBookById(int id) {
-        return books.stream()
-                .filter(book -> book.getId() == id)
-                .findFirst()
-                .orElse(null);
-    }
-
-    public void updateBook(int id, Book updatedBook) {
-        books.stream()
-                .filter(book -> book.getId() == id)
-                .findFirst()
-                .ifPresent(book -> {
-                    book.setTitle(updatedBook.getTitle());
-                    book.setAuthor(updatedBook.getAuthor());
-                });
-    }
-    
-    public void deleteBook(int id) {
-        books.removeIf(book -> book.getId() == id);
-    }
+public interface BookService {
+    List<Book> getAllBooks();
+    Book getBookById(int id);
+    void addBook(Book book);
+    void updateBook(int id, Book updatedBook);
+    void deleteBook(int id);
 }
